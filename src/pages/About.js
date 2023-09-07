aa13b3ad6ddb6380b5bf424660c7483bd20d2441
Taraansh <taraanshnandwani123@gmail.com>
2023-09-07 16:23:55 +0530

    Updated Layout tag

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout/Layout";
 
 const About = () => {
   return (
-    <Layout>
+    <Layout title="About Us - ShopMonk" description="Read About Us Here">
       <h1>About</h1>
     </Layout>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout/Layout";
 
 const Contact = () => {
   return (
-    <Layout>
+    <Layout title="Contact Us - ShopMonk" description="Contact Us using these following handles.">
       <h1>Contact Page</h1>
     </Layout>
   );
