a891fe5afae87ca535fa767188fe67cd7fde092e
Taraansh <taraanshnandwani123@gmail.com>
2023-09-12 16:58:41 +0530

    Added toaster

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,32 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from 'react-hot-toast';
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
-    <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-        <meta name="author" content={author} />
-        <title>{title}</title>
-      </Helmet>
-      <Header />
-      <main style={{ height: "81vh" }}>{children}</main>
-      <Footer />
-    </div>
+    <HelmetProvider>
+      <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content={author} />
+          <title>{title}</title>
+        </Helmet>
+        <div id="body-container">
+          <Header />
+          <main id="main-body">
+            <Toaster />
+            {children}
+          </main>
+          {/* <main style={{ minHeight: "100vh" }}>{children}</main> */}
+          <Footer />
+        </div>
+      </div>
+    </HelmetProvider>
   );
 };
 
