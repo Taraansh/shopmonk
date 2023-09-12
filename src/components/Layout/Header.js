a52cc020a5a3970223bebe082cd3a63dd2b570a9
Taraansh <taraanshnandwani123@gmail.com>
2023-09-12 16:58:27 +0530

    Fixed header for small and large screen

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../../styles/Header.css";
-import {GiShoppingBag} from "react-icons/gi"
 
 const Header = () => {
   const path = useLocation();
@@ -10,11 +9,13 @@ const Header = () => {
     <nav>
       <div id="header">
           <Link className="color" id="main-icon" to="/">
-          <GiShoppingBag/>
           ShopMonk
           </Link>
 
         <ul className="list">
+        <div className="dropdown">
+        <span className="dropbtn"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></span>
+        <div className="dropdown-content">
           <Link className={`list-item color hover-item ${path.pathname === "/"? 'active':``}`} to="/">
             <li>Home</li>
           </Link>
@@ -36,6 +37,8 @@ const Header = () => {
           <Link className={`list-item color hover-item ${path.pathname === "/cart"? 'active':``}`} to="/cart">
             <li>Cart {0}</li>
           </Link>
+        </div>
+        </div>
         </ul>
       </div>
     </nav>
