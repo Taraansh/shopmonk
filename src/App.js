a031a4ca50aadaede5ffa88ef7a4e6693c5c0877
Taraansh <taraanshnandwani123@gmail.com>
2023-09-13 20:21:28 +0530

    Added dashboard and made it private

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import Policy from "./pages/Policy";
 import PageNotFound from "./pages/PageNotFound";
 import Signup from "./pages/Auth/Signup";
 import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
+    <Toaster />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute/>}>
+          <Route path="" element={<Dashboard/>}/>
+        </Route>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/policy" element={<Policy />} />
