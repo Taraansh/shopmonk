6e2b643e05202c134ca522c3f802a0b6cee6c317
Taraansh <taraanshnandwani123@gmail.com>
2023-09-13 20:23:55 +0530

    Added Context

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../components/Layout/Layout";
+import { AuthContext } from "../context/auth";
 
 const HomePage = () => {
+  const { auth } = useContext(AuthContext);
+
   return (
     <Layout>
       <h1>Homepage</h1>
+      <p>Hello</p>
+      <p>{JSON.stringify(auth, null, 4)}</p>
     </Layout>
   );
 };
