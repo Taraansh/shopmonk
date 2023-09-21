33f9edfdd14572a9e9c72a54d5ab46f16c842fdb
Taraansh <taraanshnandwani123@gmail.com>
2023-09-21 11:02:51 +0530

    Made route private

--- src/App.js
+++ src/App.js
@@ -8,17 +8,22 @@ import Signup from "./pages/Auth/Signup";
 import Login from "./pages/Auth/Login";
 import Dashboard from "./pages/user/Dashboard";
 import PrivateRoute from "./components/Routes/Private";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import ForgotPassword from "./pages/Auth/ForgotPassword";
+import AdminRoute from "./components/Routes/AdminRoute";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
 
 function App() {
   return (
     <>
-    <Toaster />
+      <Toaster />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<PrivateRoute/>}>
-          <Route path="" element={<Dashboard/>}/>
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="/dashboard/user" element={<Dashboard />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="/dashboard/admin" element={<AdminDashboard />} />
         </Route>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
