c2d2661a9686be879bfa3ed14b989ede20aacd34
Taraansh <taraanshnandwani123@gmail.com>
2023-09-21 11:03:18 +0530

    Fixed

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,7 +1,5 @@
 import React from "react";
-import "../../styles/Footer.css";
 import { Link } from "react-router-dom";
-import "../../styles/Footer.css";
 
 const Footer = () => {
   return (
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,45 +5,114 @@ import { AuthContext } from "../../context/auth";
 
 const Header = () => {
   const path = useLocation();
-  const {user, details, handleLogout}=useContext(AuthContext)
+  const { user, details, handleLogout } = useContext(AuthContext);
 
   return (
     <nav>
       <div id="header">
-          <Link className="color" id="main-icon" to="/">
+        <Link className="color" id="main-icon" to="/">
           ShopMonk
-          </Link>
+        </Link>
 
         <ul className="list">
-        <div className="dropdown">
-        <span className="dropbtn"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></span>
-        <div className="dropdown-content">
-          <Link className={`list-item color hover-item ${path.pathname === "/"? 'active':``}`} to="/">
-            <li>Home</li>
-          </Link>
-          <Link className={`list-item color hover-item ${path.pathname === "/category"? 'active':``}`} to="/category">
-            <li>Category</li>
-          </Link>
-          
-          {user ? 
-          (
-            <><Link className={`list-item color hover-item ${path.pathname === "/dashboard" ? 'active' : ``}`} to="/dashboard">
-                    <li>Dashboard</li>
-                  </Link><Link onClick={handleLogout} className={`list-item color hover-item ${path.pathname === "/login" ? 'active' : ``}`} to="/login">
-                      <li>Logout ({details.name})</li>
-                    </Link></>):
-          (<><Link className={`list-item color hover-item ${path.pathname === "/login" ? 'active' : ``}`} to="/login">
-          <li>Login</li>
-          </Link><Link className={`list-item color hover-item ${path.pathname === "/signup" ? 'active' : ``}`} to="/signup">
-          <li>Signup</li>
-          </Link></>)
-          }
-          
-          <Link className={`list-item color hover-item ${path.pathname === "/cart"? 'active':``}`} to="/cart">
-            <li>Cart {0}</li>
-          </Link>
-        </div>
-        </div>
+          <div className="dropdown">
+            <span className="dropbtn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+              </svg>
+            </span>
+            <div className="dropdown-content">
+              <Link
+                className={`list-item color hover-item ${
+                  path.pathname === "/" ? "active" : ``
+                }`}
+                to="/"
+              >
+                <li>Home</li>
+              </Link>
+              <Link
+                className={`list-item color hover-item ${
+                  path.pathname === "/category" ? "active" : ``
+                }`}
+                to="/category"
+              >
+                <li>Category</li>
+              </Link>
+
+              {user ? (
+                <>
+                  <div className="login-dropdown">
+                    <span className="login-dropbtn list-item color hover-item">
+                      {details?.name
+                        ? details.name
+                        : localStorage.getItem("name")}
+                    </span>
+                    <div className="login-dropdown-content">
+                      <Link
+                        className={`list-item color ${
+                          path.pathname === "/dashboard" ? "active" : ``
+                        }`}
+                        to={`/dashboard/${
+                          details?.role === 1 ? "admin" : "user"
+                        }`}
+                      >
+                        <li>Dashboard</li>
+                      </Link>
+                      <Link
+                        onClick={handleLogout}
+                        className={`list-item color ${
+                          path.pathname === "/login" ? "active" : ``
+                        }`}
+                        to="/login"
+                      >
+                        <li>Logout</li>
+                      </Link>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <Link
+                    className={`list-item color hover-item ${
+                      path.pathname === "/login" ? "active" : ``
+                    }`}
+                    to="/login"
+                  >
+                    <li>Login</li>
+                  </Link>
+                  <Link
+                    className={`list-item color hover-item ${
+                      path.pathname === "/signup" ? "active" : ``
+                    }`}
+                    to="/signup"
+                  >
+                    <li>Signup</li>
+                  </Link>
+                </>
+              )}
+
+              <Link
+                className={`list-item color hover-item ${
+                  path.pathname === "/cart" ? "active" : ``
+                }`}
+                to="/cart"
+              >
+                <li>Cart {0}</li>
+              </Link>
+            </div>
+          </div>
         </ul>
       </div>
     </nav>
