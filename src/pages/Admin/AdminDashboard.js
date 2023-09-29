e8b712dce1f20f483650c90b4e59b8fdc4412612
Taraansh <taraanshnandwani123@gmail.com>
2023-09-29 11:52:47 +0530

    Added admin info

--- src/pages/Admin/AdminDashboard.js
+++ src/pages/Admin/AdminDashboard.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../../components/Layout/Layout";
-import AdminMenu from "../../components/Layout/AdminMenu";
+import AdminMenu from "./AdminMenu";
+import { AuthContext } from "../../context/auth";
 
 const AdminDashboard = () => {
+  const {details} = useContext(AuthContext)
   return (
     <Layout>
-      <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", marginTop: '1rem' }}>
         <div>
           <AdminMenu />
         </div>
-        <div id="content">content</div>
+        <div className="admin-content">
+          <p>Admin Name: {details?.name}</p>
+          <p>Admin Email: {details?.email}</p>
+          <p>Admin Contact: {details?.phone}</p>
+          </div>
       </div>
     </Layout>
   );
