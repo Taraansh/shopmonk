24d5c25b59af34f0d19188878bef872a925eac7a
Taraansh <taraanshnandwani123@gmail.com>
2023-09-29 19:03:07 +0530

    Added User Dashboard

--- src/pages/user/Dashboard.js
+++ src/pages/user/Dashboard.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../../components/Layout/Layout";
+import { AuthContext } from "../../context/auth";
+import UserMenu from "./UserMenu";
 
 const Dashboard = () => {
+  const {details} = useContext(AuthContext)
+
   return (
     <Layout title="Dashboard - ShopMonk">
-      <h1>Dashboard</h1>
+      <div style={{ display: "flex", marginTop: '1rem' }}>
+        <div>
+          <UserMenu />
+        </div>
+        <div className="admin-content">
+          <p>User Name: {details?.name}</p>
+          <p>User Email: {details?.email}</p>
+          <p>User Contact: {details?.phone}</p>
+          </div>
+      </div>
     </Layout>
   );
 };
